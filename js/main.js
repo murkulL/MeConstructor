fa7d5710171267736c324cc0370b0c6f1186b621
murkulL <olegov23445@gamil.com>
2023-08-16 16:38:14 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,71 +1,47 @@
-$(document).ready(function() {
-  $('.articles__items').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    dots: true,
-    autoplay: true,
-    mobileFirst: true,
-    responsive: [
-      {
-        breakpoint: 700,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
+
+// -------------------------------  bootstrap
+
+const handleCarouselItems = () => {
+  const carouselItem = document.querySelector('.plans .carousel-item');
+  if (carouselItem) {
+    const carouselItems = document.querySelectorAll('.plans .carousel-item');
+    const mobileContainers = document.querySelectorAll('.mobile-container');
+
+    if (window.innerWidth > 500) {
+      mobileContainers.forEach((container) => {
+        container.classList.remove('mobile-container');
+      });
+      carouselItems.forEach((item) => {
+        item.classList.remove('active');
+      });
+    } else {
+      carouselItem.classList.add('active');
+      mobileContainers.forEach((container) => {
+        container.classList.add('mobile-container');
+      });
+    }
+  }
+};
+
+handleCarouselItems();
+window.addEventListener('resize', handleCarouselItems);
+
+// ------------------------------------------------------- owl carousel
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:0,
+  nav:false,
+  dots: true,
+  responsive:{
+      0:{
+          items:1
       },
-      {
-        breakpoint: 1000,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
+      800:{
+          items:2
+      },
+      1050:{
+          items:3
       }
-    ]
-  });
-
-  initPlansSlider();
-});
-
-let plansSlider;
-
-function initPlansSlider() {
-  plansSlider = $('.slider2').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    dots: false,
-    autoplay: false,
-    mobileFirst: true,
-    responsive: [
-      {
-        breakpoint: 700,
-        settings: "unslick" 
-      }
-    ]
-  });
-}
-
-function destroyPlansSlider() {
-  if (plansSlider) {
-    plansSlider.slick("unslick");
-    plansSlider = null;
   }
-}
-
-function checkWindowWidth() {
-  if ($(window).width() >= 700) {
-    destroyPlansSlider();
-  } else {
-    initPlansSlider();
-  }
-}
-
-$(document).ready(function() {
-  initPlansSlider();
-  checkWindowWidth();
-});
-
-$(window).on('resize', function() {
-  checkWindowWidth();
-});
+})
